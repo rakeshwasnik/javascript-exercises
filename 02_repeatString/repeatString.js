1d85922a0debd46996021c740c18fc2752fdaaa7
rakeshwasnik <rbwasnik99@gmail.com>
2022-04-11 20:20:26 +0530

    .

--- 02_repeatString/repeatString.js
+++ 02_repeatString/repeatString.js
@@ -1,5 +1,10 @@
-const repeatString = function() {
-
+const repeatString = function(text, times) {
+    if(times<0){
+        return 'ERROR';
+    }
+    else {
+        return text.repeat(times); 
+    }
 };
 
 // Do not edit below this line
--- 03_reverseString/reverseString.js
+++ 03_reverseString/reverseString.js
@@ -1,5 +1,9 @@
-const reverseString = function() {
-
+const reverseString = function(str) {
+    reverseSTR = '';
+    for (i = str.length-1; i>=0; i--){
+        reverseSTR = reverseSTR+str[i];
+    }
+    return reverseSTR;
 };
 
 // Do not edit below this line
