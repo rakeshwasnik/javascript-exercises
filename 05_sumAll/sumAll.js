7e4f8f2994b116e524b3461f20555f41e324d7b1
rakeshwasnik <rbwasnik99@gmail.com>
2022-04-12 13:48:22 +0530

    .

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,24 @@
-const sumAll = function() {
+const sumAll = function(start, end) {
+    let sum = 0;
 
+    if(start<0 || end<0 || (typeof start) != "number" || (typeof end) != "number") {
+        return 'ERROR';
+    }
+    else if(start === end) {
+        sum = start;
+    }
+    else if(start<end) {
+        for(i=start; i<=end; i++) {
+            sum = sum + i;
+        }
+    }
+    else if(start>end) {
+        for(i=end; i<=start; i++) {
+            sum = sum + i;
+        }
+    }
+    
+    return sum;
 };
 
 // Do not edit below this line
